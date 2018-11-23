86b0088f8164591131c3f63ce1f58f9c6843326d
Luis Miguel <lmarinl1@outlook.com>
2018-11-23 16:11:24 -0500

    Counting Proteins in a serial way

--- src/serial/Counting_Proteins.py
+++ src/serial/Counting_Proteins.py
@@ -6,11 +6,16 @@ from datetime import datetime
 
 # Map declaration
 m = {'GCT':'A','GCC':'A','GCA':'A','GCG':'A','CGT':'R','CGC':'R','CGA':'R','CGG':'R','AGA':'R','AGG':'R','AAT':'N','AAC':'N','GAT':'D','GAC':'D','TGT':'C','TGC':'C','CAA':'Q','CAG':'Q','GAA':'E','GAG':'E','GGT':'G','GGC':'G','GGA':'G','GGG':'G','CAT':'H','CAC':'H','ATT':'I','ATC':'I','ATA':'I','ATG':'1','TTA':'L','TTG':'L','CTT':'L','CTC':'L','CTA':'L','CTG':'L','AAA':'K','AAG':'K','ATG':'M','TTT':'F','TTC':'F','CCT':'P','CCC':'P','CCA':'P','CCG':'P','TCT':'S','TCC':'S','TCA':'S','TCG':'S','AGT':'S','AGC':'S','ACT':'T','ACC':'T','ACA':'T','ACG':'T','TGG':'W','TAT':'Y','TAC':'Y','GTT':'V','GTC':'V','GTA':'V','GTG':'V','TAA':'0','TGA':'0','TAG':'0'}
+n = {'A':0,'R':0,'N':0,'D':0,'C':0,'Q':0,'E':0,'G':0,'H':0,'I':0,'1':0,'L':0,'K':0,'M':0,'F':0,'P':0,'S':0,'T':0,'W':0,'Y':0,'V':0,'0':0,'X':0 }
+
 
 def getCodon(tri):
     if m.has_key(tri):
-        return m[tri]
+        a = m[tri]
+        n[a] += 1 
+        return a
     else:
+        n['X'] += 1
         return 'X'
     
 
@@ -43,12 +48,18 @@ def countNucleotides(folderName):
         fasta.close()
         print " --- Generated "+filefna+".proteins --- "+str((datetime.now()-instanteinicial).seconds)+" sec"
         
+def printCounting():
+    print 'Results ----------------------------------------------------------------------------------------'
+    for i in n:
+        print "> "+i+": "+str(n[i])
+
 def main():
     if len(sys.argv) != 2:
         raise Exception('Usage python Counting_DNA_Nucleotides.py  <FolderName>')
     
     if not isfile(join(sys.argv[1])):
         countNucleotides(sys.argv[1])
+        printCounting()
     else: 
         raise Exception('<FolderName> must be a Folder')
 
