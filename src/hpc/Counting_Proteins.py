b527becc2564848e75d7ee99f88b29443c25c0f7
jhoyosg5 <33663056+jhoyosg5@users.noreply.github.com>
2018-11-24 13:54:12 -0500

    changes in serial and parallel

--- src/hpc/Counting_Proteins.py
+++ src/hpc/Counting_Proteins.py
@@ -4,12 +4,12 @@ from os.path import isfile, join
 from datetime import datetime
 from mpi4py import MPI 
 
-# Map declaration
+# Map declaration this will list all the amino acids with their respective codon
 m = {'GCT':'A','GCC':'A','GCA':'A','GCG':'A','CGT':'R','CGC':'R','CGA':'R','CGG':'R','AGA':'R','AGG':'R','AAT':'N','AAC':'N','GAT':'D','GAC':'D','TGT':'C','TGC':'C','CAA':'Q','CAG':'Q','GAA':'E','GAG':'E','GGT':'G','GGC':'G','GGA':'G','GGG':'G','CAT':'H','CAC':'H','ATT':'I','ATC':'I','ATA':'I','ATG':'1','TTA':'L','TTG':'L','CTT':'L','CTC':'L','CTA':'L','CTG':'L','AAA':'K','AAG':'K','ATG':'M','TTT':'F','TTC':'F','CCT':'P','CCC':'P','CCA':'P','CCG':'P','TCT':'S','TCC':'S','TCA':'S','TCG':'S','AGT':'S','AGC':'S','ACT':'T','ACC':'T','ACA':'T','ACG':'T','TGG':'W','TAT':'Y','TAC':'Y','GTT':'V','GTC':'V','GTA':'V','GTG':'V','TAA':'0','TGA':'0','TAG':'0'}
 #n = {'A':0,'R':0,'N':0,'D':0,'C':0,'Q':0,'E':0,'G':0,'H':0,'I':0,'1':0,'L':0,'K':0,'M':0,'F':0,'P':0,'S':0,'T':0,'W':0,'Y':0,'V':0,'0':0,'X':0 }
 
 
-def getCodon(tri, n):
+def getCodon(tri, n): #This method returns the amino acid that belongs to a codon, if the codon is not identified it will return an X
     if tri in m:
         a = m[tri]
         n[a] += 1 
@@ -41,13 +41,14 @@ def reviewFile(folderName, filefna, n):
     print(" --- Generated "+filefna+".proteins --- "+str((datetime.now()-instanteinicial).seconds)+" sec")
     sys.stdout.flush()
     
+# This method will print the countingof the amino acids
 def printCounting(n):
     print('Results ----------------------------------------------------------------------------------------')
     for i in n:
         print("> "+i+": "+str(n[i]))
     sys.stdout.flush()
         
-
+# Method that will count how many amino acid are
 def countNucleotides(folderName):
     """ Print the nucleotides count (A: Adenine, C: Citocine, G: Guanine, T: Timminine) and (N: not identified) """
     archivosFna = [filefna for filefna in listdir(folderName) if filefna[-3:] == ".fa"]
--- src/serial/Counting_Proteins.py
+++ src/serial/Counting_Proteins.py
@@ -4,11 +4,11 @@ from os import listdir
 from os.path import isfile, join
 from datetime import datetime
 
-# Map declaration
+# Map declaration this will list all the amino acids with their respective codon
 m = {'GCT':'A','GCC':'A','GCA':'A','GCG':'A','CGT':'R','CGC':'R','CGA':'R','CGG':'R','AGA':'R','AGG':'R','AAT':'N','AAC':'N','GAT':'D','GAC':'D','TGT':'C','TGC':'C','CAA':'Q','CAG':'Q','GAA':'E','GAG':'E','GGT':'G','GGC':'G','GGA':'G','GGG':'G','CAT':'H','CAC':'H','ATT':'I','ATC':'I','ATA':'I','ATG':'1','TTA':'L','TTG':'L','CTT':'L','CTC':'L','CTA':'L','CTG':'L','AAA':'K','AAG':'K','ATG':'M','TTT':'F','TTC':'F','CCT':'P','CCC':'P','CCA':'P','CCG':'P','TCT':'S','TCC':'S','TCA':'S','TCG':'S','AGT':'S','AGC':'S','ACT':'T','ACC':'T','ACA':'T','ACG':'T','TGG':'W','TAT':'Y','TAC':'Y','GTT':'V','GTC':'V','GTA':'V','GTG':'V','TAA':'0','TGA':'0','TAG':'0'}
 n = {'A':0,'R':0,'N':0,'D':0,'C':0,'Q':0,'E':0,'G':0,'H':0,'I':0,'1':0,'L':0,'K':0,'M':0,'F':0,'P':0,'S':0,'T':0,'W':0,'Y':0,'V':0,'0':0,'X':0 }
 
-
+#This method returns the amino acid that belongs to a codon, if the codon is not identified it will return an X
 def getCodon(tri):
     if m.has_key(tri):
         a = m[tri]
@@ -18,7 +18,7 @@ def getCodon(tri):
         n['X'] += 1
         return 'X'
     
-
+# Method that will count how many amino acid are
 def countNucleotides(folderName):
     """ Print the nucleotides count (A: Adenine, C: Citocine, G: Guanine, T: Timminine) and (N: not identified) """
     archivosFna = [filefna for filefna in listdir(folderName) if filefna[-3:] == ".fa"]
@@ -48,6 +48,7 @@ def countNucleotides(folderName):
         fasta.close()
         print " --- Generated "+filefna+".proteins --- "+str((datetime.now()-instanteinicial).seconds)+" sec"
         
+# This method will print the countingof the amino acids
 def printCounting():
     print 'Results ----------------------------------------------------------------------------------------'
     for i in n:
